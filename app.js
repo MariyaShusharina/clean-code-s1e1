1d488159a2f997920216a7a8489eec6022809943
Mariya Shusharina <mariya.shushar@gmail.com>
2024-12-17 05:43:27 +0300

    fix(basic-3.1, 3.2, 3.3, 3.4; extended-2.1): make selector names to be not as ids

--- app.js
+++ app.js
@@ -10,8 +10,8 @@
 
 var taskInput=document.getElementById("new-task");//Add a new task.
 var addButton=document.querySelector(".add-element__btn");//first button
-var incompleteTaskHolder=document.querySelector("incomplete-tasks");//ul of #incompleteTasks
-var completedTasksHolder=document.querySelector("completed-tasks");//completed-tasks
+var incompleteTaskHolder=document.querySelector(".incomplete-tasks");//ul of #incompleteTasks
+var completedTasksHolder=document.querySelector(".completed-tasks");//completed-tasks
 
 
 //New task list item
